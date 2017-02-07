9edfb8422c6c808621ca1d7bca7ab2b4ad190de8
Étienne Bérubé <tiennebrb@gmail.com>
2017-02-07 15:33:50 -0500

    Main Transitions done (test)
    Transitions between main and login
    basic

--- Champs_Mood/app/src/main/java/ultramirinc/champs_mood/LoginActivity.java
+++ Champs_Mood/app/src/main/java/ultramirinc/champs_mood/LoginActivity.java
@@ -3,6 +3,7 @@ package ultramirinc.champs_mood;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -66,6 +67,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Intent intent = getIntent();
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
--- Champs_Mood/app/src/main/java/ultramirinc/champs_mood/MainActivity.java
+++ Champs_Mood/app/src/main/java/ultramirinc/champs_mood/MainActivity.java
@@ -1,19 +1,50 @@
 package ultramirinc.champs_mood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean isConnected;
+    private boolean isConnected = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(isConnected){
+
+        /*
+        Button bTrue = (Button) findViewById(R.id.btrue);
+        Button bFalse = (Button) findViewById(R.id.bfalse);
+        */
+
+
+        if(!isConnected){
             //Start Login Activity blblblbl
+            startLoginActivity();
         }
+    }
+
+    public void startLoginActivityView(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+
+        startActivity(intent);
+    }
+
+    public void startLoginActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
+
+        startActivity(intent);
+    }
+
+    public boolean isConnected() {
+        return isConnected;
+    }
+
+    public void setConnected(boolean connected) {
+        isConnected = connected;
     }
 }
