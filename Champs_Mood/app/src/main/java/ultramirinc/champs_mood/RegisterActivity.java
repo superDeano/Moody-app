6980cfd60e2afeec70657c9a4eb508e6de4767ef
Étienne Bérubé <tiennebrb@gmail.com>
2017-03-21 13:33:41 -0400

    Finished RegisterActivity Layout
    self-explanatory

--- Champs_Mood/app/src/main/java/ultramirinc/champs_mood/RegisterActivity.java
+++ Champs_Mood/app/src/main/java/ultramirinc/champs_mood/RegisterActivity.java
@@ -358,7 +358,7 @@ public class RegisterActivity extends AppCompatActivity implements LoaderManager
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.blank_toolbar, menu);
         return true;
     }
 
