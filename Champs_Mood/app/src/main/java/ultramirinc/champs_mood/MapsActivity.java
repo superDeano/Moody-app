26510d57eb6119b92ab2928ac9b241ae684c23e8
Étienne Bérubé <tiennebrb@gmail.com>
2017-03-26 17:54:31 -0400

    Worked on homePage
    Tried maps

--- Champs_Mood/app/src/main/java/ultramirinc/champs_mood/MapsActivity.java
+++ Champs_Mood/app/src/main/java/ultramirinc/champs_mood/MapsActivity.java
@@ -38,14 +38,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        setContentView(R.layout.activity_maps2);
+        setContentView(R.layout.fragment_homepage);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        SupportMapFragment mMap = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
 
-        createGoogleMapClient();
-
-        mapFragment.getMapAsync(this);
+        createGoogleMapClient(); // <- moi
+        Log.d("debug", "in");
+        mMap.getMapAsync(this);
 
     }
 
@@ -68,18 +68,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .build();
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Log.d("debug", "in");
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
@@ -105,6 +96,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             LatLng me = new LatLng(Double.parseDouble(mLatitudeText), Double.parseDouble(mLongitudeText));
             mMap.addMarker(new MarkerOptions().position(me).title("me"));
         }
+
     }
 
     private void checkPermission(){
