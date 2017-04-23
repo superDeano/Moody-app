d383b3710c9882f5c9910eff0a899bef49bba1c5
wilpl <williampl@hotmail.com>
2017-04-23 18:20:50 -0400

    ca marche pas tabarnak

--- Champs_Mood/app/src/main/java/ultramirinc/champs_mood/fragments/FriendsFragment.java
+++ Champs_Mood/app/src/main/java/ultramirinc/champs_mood/fragments/FriendsFragment.java
@@ -1,26 +1,25 @@
 package ultramirinc.champs_mood.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
-
+import ultramirinc.champs_mood.FriendProfilActivity;
 import ultramirinc.champs_mood.R;
 
 /**
  * Created by Étienne Bérubé on 2017-03-23.
  */
 
-public class FriendsFragment extends Fragment {
+public class FriendsFragment extends Fragment{
 
     private RecyclerView recyclerView;
     private List<Person> friends = new ArrayList<>();
@@ -34,47 +33,25 @@ public class FriendsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         View view = inflater.inflate(R.layout.fragment_friends, container, false);
-
-
-
-        /*
-        for(MyObject m : cities){
-            Log.d("debug", m.getName());
-        }
-        */
-
-
-
-
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
-
         recyclerView.setLayoutManager(new LinearLayoutManager(context));
-
         recyclerView.setAdapter(new MyAdapter(friends));
 
-        return view;
-    }
+        TextView textName = (TextView)view.findViewById(R.id.name);
 
-
-    @Override
-    public void onResume(){
-        super.onResume();
-        Handler h = new Handler();
-        /*h.postDelayed(new Runnable() {
+        /*textName.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void run() {
-
-
-
-                h.postDelayed(this, time)
+            public void onClick(View v){
+                Intent intent = new Intent(getActivity(), FriendProfilActivity.class);
+                startActivity(intent);
             }
-        }, 5 min);
-        */
+        });*/
 
-        h.removeCallbacksAndMessages(null);
+        return view;
+
     }
-
 
     private void addFriends() {
         friends.add(new Person("Owen Bross", "Hungry", "In Break", true));
@@ -83,8 +60,6 @@ public class FriendsFragment extends Fragment {
         friends.add(new Person("Dany", "Programming", "Break in 1 hour", true));
         friends.add(new Person("Alex", "Studying", "Break in 1.5 hour", true));
         friends.add(new Person("Ming", "Chilling", "In Break", true));
-
     }
 
 }
-
